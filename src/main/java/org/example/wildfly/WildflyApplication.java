12c8c0fd5cb6136eeae0998eba66e0d19ff73c56
Julien Defferrard <julien.defferrard@morphean.com>
2024-08-23 08:47:50 +0200

    manually applying read only flag to connection, but still doesn't work

--- src/main/java/org/example/wildfly/WildflyApplication.java
+++ src/main/java/org/example/wildfly/WildflyApplication.java
@@ -2,8 +2,10 @@ package org.example.wildfly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class WildflyApplication {
 
 	public static void main(String[] args) {
